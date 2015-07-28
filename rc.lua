c0f2a12aa0cd851a645b31b6c5d15448f7072651
Kun Zhang <zhangkun@google.com>
2015-07-28 14:24:50 -0700

    Add tag 10 (Mod+0)

--- rc.lua
+++ rc.lua
@@ -71,7 +71,7 @@ layouts =
 tags = {}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }, s, layouts[1])
 end
 -- }}}
 
@@ -294,10 +294,10 @@ clientkeys = awful.util.table.join(
         end)
 )
 
--- Compute the maximum number of digit we need, limited to 9
+-- Compute the maximum number of digit we need, limited to 10
 keynumber = 0
 for s = 1, screen.count() do
-   keynumber = math.min(9, math.max(#tags[s], keynumber));
+   keynumber = math.min(10, math.max(#tags[s], keynumber));
 end
 
 -- Bind all key numbers to tags.
