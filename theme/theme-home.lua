3aec734c58cf8edd25fc38f6f20be9ee63b30433
Kun Zhang <zhangkun@google.com>
2015-11-13 19:59:56 -0800

    Fix wallpaper of home profile

--- theme/theme-home.lua
+++ theme/theme-home.lua
@@ -1,5 +1,5 @@
 theme.font = "sans 10"
 theme.menu_height = "24"
-theme.wallpaper_cmd = theme_dir .. "background-foggy-forest-1920x1200.jpg"
+theme.wallpaper = theme_dir .. "background-foggy-forest-1920x1200.jpg"
 
 return theme
