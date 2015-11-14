2440d4136641ed2d5257022e49379eba7bc3130d
Kun Zhang <zhangkun@google.com>
2015-11-14 09:38:11 -0800

    Increase the timeout of autostart notification to 60s

--- bin/start_if_absent.sh
+++ bin/start_if_absent.sh
@@ -6,7 +6,7 @@ RESULT=$(ps -u $USER | awk '{print $NF}' | fgrep "$1")
 
 if [ -z "$RESULT" ]; then
   $2 &
-  echo "naughty.notify({text = \"<b>$2</b> started\"})" | awesome-client
+  echo "naughty.notify({text = \"<b>$2</b> started\", timeout = 30})" | awesome-client
 else
-  echo "naughty.notify({text = \"<b>$2</b> already running\"})" | awesome-client
+  echo "naughty.notify({text = \"<b>$2</b> already running\", timeout = 30})" | awesome-client
 fi
