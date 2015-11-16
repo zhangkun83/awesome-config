9635b3f4116f6a19e34f8c51850920d46dfd40f3
Kun Zhang <zhangkun@google.com>
2015-11-16 09:08:22 -0800

    Disable Mod+T as the toggle key for ontop

--- rc.lua
+++ rc.lua
@@ -434,7 +434,7 @@ clientkeys = awful.util.table.join(
     awful.key({ modkey,  },          "f",      awful.client.floating.toggle                     ),
     awful.key({ modkey, "Control" }, "space", function (c) c:swap(awful.client.getmaster()) end),
     awful.key({ modkey,           }, "o",      awful.client.movetoscreen                        ),
-    awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end),
+    --awful.key({ modkey,           }, "t",      function (c) c.ontop = not c.ontop            end),
     awful.key({ modkey,           }, "n",
         function (c)
             -- The client currently has the input focus, so it cannot be
