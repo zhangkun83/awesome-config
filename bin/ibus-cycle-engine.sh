bb9882052f51d8f1803146f993b988332d7b24f0
Kun Zhang <zhangkun@google.com>
2016-01-11 09:52:39 -0800

    Change ibus hotkeys.
    Give up Ctrl+Space for Emacs. Use Mod4+F1 and Mod4+F2 to select input
    methods.

--- bin/ibus-cycle-engine.sh
+++ bin/ibus-cycle-engine.sh
@@ -11,20 +11,25 @@ get_current_engine() {
 }
 
 declare -a ENGINES=('xkb:us::eng' 'pinyin')
-NUM_ENGINES=${#ENGINES[@]}
-CURRENT_ENGINE=$(get_current_engine) || exit 1
 
-let LAST_INDEX=NUM_ENGINES-1
-INDEX=0
-while [ $INDEX -lt $LAST_INDEX ]; do
-  if [ "$CURRENT_ENGINE" == ${ENGINES[$INDEX]} ]; then
-    break
-  fi
-  let INDEX+=1
-done
-let INDEX+=1
-if [ $INDEX -gt $LAST_INDEX ]; then
+if [ "$#" -lt 1 ]; then
+  NUM_ENGINES=${#ENGINES[@]}
+  CURRENT_ENGINE=$(get_current_engine) || exit 1
+
+  let LAST_INDEX=NUM_ENGINES-1
   INDEX=0
+  while [ $INDEX -lt $LAST_INDEX ]; do
+    if [ "$CURRENT_ENGINE" == ${ENGINES[$INDEX]} ]; then
+      break
+    fi
+    let INDEX+=1
+  done
+  let INDEX+=1
+  if [ $INDEX -gt $LAST_INDEX ]; then
+    INDEX=0
+  fi
+else
+  INDEX=$1
 fi
 
 ibus engine ${ENGINES[$INDEX]}
