291954e304b94e72efa21aa8c9514df62b0de539
Kun Zhang <zhangkun@google.com>
2016-01-19 10:43:10 -0800

    Use -drx to start ibus-daemon.
    -r replaces current ibus-daemon, so you don't have to kill it first.
    -x starts XIM server, which is needed to work in Emacs.

--- bin/restart_ibus.sh
+++ bin/restart_ibus.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-killall ibus-daemon &> /dev/null
-sleep 5
-ibus-daemon -d
+# -x is needed to work in Emacs
+ibus-daemon -drx
 echo "naughty.notify({text = \"<b>ibus-daemon</b> (re)started\"})" | awesome-client
