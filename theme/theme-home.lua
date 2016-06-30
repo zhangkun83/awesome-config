7836970cd31ee822f4b37d9f00a444e7898f0aee
Kun Zhang <zhangkun@google.com>
2016-06-30 14:04:04 -0700

    Switch to Arial font

--- theme/theme-home.lua
+++ theme/theme-home.lua
@@ -1,4 +1,4 @@
-theme.font = "Aegean Bold 11.5"
+theme.font = "Arial 12"
 theme.menu_height = "24"
 
 return theme
--- theme/theme-work.lua
+++ theme/theme-work.lua
@@ -1,4 +1,4 @@
-theme.font = "Aegean Bold 12"
+theme.font = "Arial 12"
 theme.menu_height = "28"
 
 return theme
