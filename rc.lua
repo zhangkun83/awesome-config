85fa419b113641af5a9a7f9079883891619dd51e
Kun Zhang <zhangkun@google.com>
2016-12-15 16:34:39 -0800

    Move volume control keys to the Fn line

--- rc.lua
+++ rc.lua
@@ -490,9 +490,9 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey }, "/", function () awful.util.spawn(cheatsheet_command) end),
     awful.key({ modkey, "Shift" }, "d", function() set_floating_for_all_clients(false) end),
     awful.key({ modkey, "Shift" }, "f", function() set_floating_for_all_clients(true) end),
-    awful.key({ modkey }, "KP_Add", function() awful.util.spawn(config_home .. "bin/volume.sh up") end),
-    awful.key({ modkey }, "KP_Subtract", function() awful.util.spawn(config_home .. "bin/volume.sh down") end),
-    awful.key({ modkey }, "KP_Multiply", function() awful.util.spawn(config_home .. "bin/volume.sh mute") end),
+    awful.key({ modkey }, "F7", function() awful.util.spawn(config_home .. "bin/volume.sh up") end),
+    awful.key({ modkey }, "F6", function() awful.util.spawn(config_home .. "bin/volume.sh down") end),
+    awful.key({ modkey }, "F5", function() awful.util.spawn(config_home .. "bin/volume.sh mute") end),
     -- As we have removed mysystray, there no easy way to tell the current ibus input engine,
     -- We intercept the ibus hotkey and switch engine manually, so that we can display the current engine
     -- as an notification.
