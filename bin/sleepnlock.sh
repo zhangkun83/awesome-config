fc23e91c78569a18153fab6f31a2452679251195
Kun Zhang <zhangkun@google.com>
2017-05-02 18:08:43 -0700

    No need to sleep after xscreensaver lock.
    xscreensaver doesn't exit until the lock is done.

--- bin/sleepnlock.sh
+++ bin/sleepnlock.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 xscreensaver-command -lock
-sleep 2
 dbus-send --system --print-reply --dest="org.freedesktop.UPower" \
           /org/freedesktop/UPower org.freedesktop.UPower.Suspend
