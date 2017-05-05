c08488891cb2f436e70b45522dedf380fefe81af
Kun Zhang <zhangkun@google.com>
2017-05-05 09:22:41 -0700

    Simply backlight.sh notification impl.

--- bin/backlight.sh
+++ bin/backlight.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-which xbacklight > /dev/null
-if [ $? -ne 0 ]; then
-  echo "naughty.notify({ preset = naughty.config.presets.critical,"\
-    "  text = \"xbacklight not installed\"})" | awesome-client
-  exit 1
-fi
-
 if [ "$1" == "up" ]; then
   xbacklight +10
 elif [ "$1" == "down" ]; then
@@ -16,8 +9,4 @@ current_level=$(xbacklight)
 current_level=$(sed -e 's/\..*$//g' <<< "$current_level")
 message="backlight: $current_level%"
 
-echo "naughty.destroy(backlightnotification)" | awesome-client
-echo "backlightnotification = naughty.notify({"\
-  "text = \"$message\","\
-  "position = \"top_right\"})" |\
-  awesome-client
+echo "backlightnotification = mynotify(\"$message\", backlightnotification)" | awesome-client
