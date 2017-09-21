1088b54892e16f3b2c9b8103535746a5bbe6cf3b
Kun Zhang <zhangkun@google.com>
2017-09-21 09:38:23 -0700

    Fine-tune compton for better performance

--- bin/post-start-commands.sh
+++ bin/post-start-commands.sh
@@ -4,7 +4,7 @@ killall xscreensaver
 xscreensaver -no-splash &
 
 killall compton
-compton -c -b -r 5 -l -7 -t -7
+compton -c -b -r 5 -l -7 -t -7 --backend glx --glx-no-stencil --glx-no-rebind-pixmap
 
 # Change key repeat rate
 xset r rate 220 30
