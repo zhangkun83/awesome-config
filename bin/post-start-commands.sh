8886757f85a941ed09388847c9718aefd5f83b07
Kun Zhang <zhangkun@google.com>
2017-09-20 23:42:02 -0700

    Fine-tune compton shadows

--- bin/post-start-commands.sh
+++ bin/post-start-commands.sh
@@ -4,7 +4,7 @@ killall xscreensaver
 xscreensaver -no-splash &
 
 killall compton
-compton -c -C -b -r 4 -l -2 -t -2
+compton -c -C -b -r 5 -l -7 -t -7
 
 # Change key repeat rate
 xset r rate 220 30
