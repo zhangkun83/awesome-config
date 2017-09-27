b8a6961f6e011013747a592eea5786f3afe66648
Kun Zhang <zhangkun@google.com>
2017-09-27 15:58:49 -0700

    Checkin terminal icon for urxvt

--- bin/urxvt.sh
+++ bin/urxvt.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 urxvtc --inputMethod ibus --font "xft:Liberation Mono:size=11" --letterSpace -1 -sr -bc -st\
-      -icon /usr/share/icons/oxygen/48x48/apps/utilities-terminal.png "$@"
+      -icon "${DIR}/terminal.png" "$@"
