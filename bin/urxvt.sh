39c26589b5c6206fbe91575f2012ac485acdfb94
Kun Zhang <zhangkun@google.com>
2017-09-27 07:58:11 -0700

    Use urxvtd to save memory

--- bin/urxvt.sh
+++ bin/urxvt.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-urxvt --inputMethod ibus --font "xft:Liberation Mono:size=11" --letterSpace -1 -sr -bc -st\
+urxvtc --inputMethod ibus --font "xft:Liberation Mono:size=11" --letterSpace -1 -sr -bc -st\
       -icon /usr/share/icons/oxygen/48x48/apps/utilities-terminal.png "$@"
