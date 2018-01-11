7e34343bd1bb80da6d99e5c048e40fd5b67c7080
Kun Zhang <zhangkun@google.com>
2018-01-10 21:30:50 -0800

    Unnecessary to set LUA_PATH in awesome.sh
    "." is already in it by default.

--- awesome.sh
+++ awesome.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-export LUA_PATH="$HOME"'/.config/awesome/?.lua'
-export LUA_PATH="$LUA_PATH"';;'
 export AWESOME_THEMES_PATH="$HOME/.config/awesome/themes"
 
 # This dumps Lua search paths for debugging
