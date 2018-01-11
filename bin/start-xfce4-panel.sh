e380264768140beb7713a4672b9e913a5b4a7a3f
Kun Zhang <zhangkun@google.com>
2018-01-10 21:21:11 -0800

    Use default theme for xfce4-panel.
    Now the applets display fine under the default bright theme.  No need
    to use the black theme.  Plus some applets don't display right on the
    black theme.

--- bin/start-xfce4-panel.sh
+++ bin/start-xfce4-panel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Make the xfce4-panel to use a dark theme which goes better with the wibox.
 # Also include other gtk theme customizations.
-GTK2_RC_FILES=/usr/share/themes/Xfce-dusk/gtk-2.0/gtkrc:$HOME/.config/awesome/gtk2rc xfce4-panel &
+xfce4-panel &
