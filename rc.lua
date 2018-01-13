88cf82c6b2b7bd7e046eb04529ba588321d1807b
Kun Zhang <zhangkun@google.com>
2018-01-12 23:30:52 -0800

    Delete unused wallpaper setting code.

--- rc.lua
+++ rc.lua
@@ -60,7 +60,7 @@ end
 -- }}}
 
 -- {{{ Variable definitions
--- Themes define colours, icons, font and wallpapers.
+-- Themes define colours, icons, font
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
 
 -- Profile can override parts of the theme
@@ -215,25 +215,7 @@ local tasklist_buttons = gears.table.join(
                                      awful.menu.toggle(tasklistMenu)
                                           end))
 
-local function set_wallpaper(s)
-    -- Wallpaper
-    if beautiful.wallpaper then
-        local wallpaper = beautiful.wallpaper
-        -- If wallpaper is a function, call it with the screen
-        if type(wallpaper) == "function" then
-            wallpaper = wallpaper(s)
-        end
-        gears.wallpaper.maximized(wallpaper, s, true)
-    end
-end
-
--- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", set_wallpaper)
-
 awful.screen.connect_for_each_screen(function(s)
-    -- Wallpaper
-    set_wallpaper(s)
-
     -- Each screen has its own tag table.
     awful.tag({ "1", "2", "3", "4", "5", "6", "7", "8", "9", "10" }, s, awful.layout.layouts[1])
 
