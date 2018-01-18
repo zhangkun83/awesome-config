eb7df121a5436447107f2f0d8752369c2c44e2be
Kun Zhang <zhangkun@google.com>
2018-01-17 19:09:57 -0800

    Use ck-launch-session to make nm-applet operable

--- awesome.sh
+++ awesome.sh
@@ -4,4 +4,6 @@ export AWESOME_THEMES_PATH="$HOME/.config/awesome/themes"
 # This dumps Lua search paths for debugging
 lua -e 'print("LUA MODULES:\n",(package.path:gsub("%;","\n\t")),"\n\nC MODULES:\n",(package.cpath:gsub("%;","\n\t")))'
 
-awesome -c $HOME/.config/awesome/rc.lua "$@"
+# ck-launch-session makes the nm-applet started by display manager
+# operable by the user.
+exec ck-launch-session awesome -c $HOME/.config/awesome/rc.lua "$@"
