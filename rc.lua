2117fcab2c312c1c5f57f8997d7a3054ccc5d633
Kun Zhang <zhangkun@google.com>
2018-04-03 10:44:51 -0700

    Make wibox ontop of windows

--- rc.lua
+++ rc.lua
@@ -239,6 +239,7 @@ awful.screen.connect_for_each_screen(function(s)
 
     -- Create the wibox
     s.mywibox = awful.wibar(awful.util.table.join({ position = "top", screen = s}, mywiboxprops))
+    s.mywibox.ontop = true
 
     -- Add widgets to the wibox
     s.mywibox:setup {
