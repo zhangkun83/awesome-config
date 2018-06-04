93ec239aed44ed7a02a74c178d87fc14cdee292a
Kun Zhang <zhangkun@google.com>
2018-06-04 14:02:06 -0700

    Revert "Remove unnecesary mouse bindings"
    This reverts commit d831b2843a1c2b0cf781d40568b7c0355bdbb0a8.

--- rc.lua
+++ rc.lua
@@ -462,7 +462,9 @@ for i = 1, 10 do
 end
 
 clientbuttons = gears.table.join(
-    awful.button({ }, 1, function (c) client.focus = c; c:raise() end))
+    awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
+    awful.button({ modkey }, 1, awful.mouse.client.move),
+    awful.button({ modkey }, 3, awful.mouse.client.resize))
 
 -- Set keys
 root.keys(globalkeys)
