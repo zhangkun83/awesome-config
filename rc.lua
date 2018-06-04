873e6b0f7ec64de018974e9907579a83889f843b
Kun Zhang <zhangkun@google.com>
2018-06-04 14:03:30 -0700

    Disable mouse resizing with Mod4 + right button

--- rc.lua
+++ rc.lua
@@ -463,8 +463,7 @@ end
 
 clientbuttons = gears.table.join(
     awful.button({ }, 1, function (c) client.focus = c; c:raise() end),
-    awful.button({ modkey }, 1, awful.mouse.client.move),
-    awful.button({ modkey }, 3, awful.mouse.client.resize))
+    awful.button({ modkey }, 1, awful.mouse.client.move))
 
 -- Set keys
 root.keys(globalkeys)
